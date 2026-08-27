db037da2e8a4caadf831d0636f06f4f0447b5e6d
xiao_dust <1599112123@qq.com>
2026-08-27 15:49:07 +0800

    chore: 移除误入的链表旧算法笔记文件夹
    merge 提交 f4575a8 合并本地旧分支 ff7f6dd（2025-12 的算法笔记）
    时，将已删除的 链表/ 文件夹重新带回了合并结果。应用主线
    本就无此目录，这里将其彻底移除。

--- 链表/ListNode.java
+++
@@ -1,7 +0,0 @@
-public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(){}
-    ListNode(int x) { val = x; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
